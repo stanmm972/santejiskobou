971cf9803d1ffedbe4290c910129d279298e7d8f
Frederic Maurice-madelon <fred972@Frederics-MacBook-Pro.local>
2022-11-21 16:30:31 -0500

    created structure site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1>Welcome</h1>
-    </div>
+      <main>
+
+      </main>
+    </>
   );
 }
 
