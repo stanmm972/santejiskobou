e4a5b5685460b430723a14e57419398ca4414d73
Frederic Maurice-madelon <fred972@Frederics-MacBook-Pro.local>
2024-01-12 15:29:35 -0500

    contact form fixed to send email to emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,91 +1,106 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import {Form, Container, Row, Col} from 'react-bootstrap';
 import Shake from 'react-reveal/Shake';
 import "./contact.css";
 import phone from "../assets/phone.png";
 
 const Contact = () => {
-    const [formState, setFormState] = useState({});
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Add your email service details
+    const serviceId = 'service_2n1a0uj';
+    const templateId = 'template_haoamon';
+    const userId = 'user_dcPheUydu1AZP5i7w5K8q';
 
     
-    const changeHandler = (event) =>{
-        setFormState({...formState, [event.target.name]: event.target.value });
-    }
+  emailjs.send(serviceId, templateId, formData, userId)
+  .then((response) => {
+    console.log('Email sent successfully:', response);
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-      const config = {
-        SecureToken : '531cf605-04db-49f7-827b-162dcdd7fcfb',
-        To : 'info@santejiskobou.com',
-        From : formState.email,
-        Subject : "Un visiteur du site web Santé Jiskobou à un commentaire",
-        Body : `Nom du visiteur: ${formState.name}, Telephone:${formState.phone}, Message du visiteur:${formState.message} `,
-    };  
-    if(window.Email){
-    window.Email.send(config).then((message) => alert("Email was sent successfully"));
-    };
-}
+    // Display a confirmation message to the user
+    alert('Email sent successfully!');
+  })
+  .catch((error) => {
+    console.error('Error sending email:', error);
+
+    // Display an error message to the user
+    alert('Error sending email. Please try again later.');
+  });
+
+    // Clear the form after submission
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    });
+  };
 
   return (
     <>
         <section className='contact' id='contact'>
             <Container>
-            <Row>
-                <Col>
-                
-                <Form onSubmit={submitHandler}>
-                    <div className="row">
-                        <div className="col-lg-12 text-center">
-                            <h2 className="section-heading text-uppercase">Contactez-Nous</h2>
-                            <h3 className="section-subheading text-muted sp">Vous avez besoins des services de Jiskobou, ou tout simplement en
-            savoir plus !?</h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12">
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate" >
-                            <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                <input className="form-control" id="name" type="text" placeholder="Votre nom *" value={formState.name || ''} name="name" required="required" data-validation-required-message="Please enter your name." onChange={changeHandler}/>
-                                <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group">
-                                <input className="form-control" id="email" type="email" placeholder="Votre courriel *" value={formState.email || ''} name="email" required="required" data-validation-required-message="Please enter your email address." onChange={changeHandler}/>
-                                <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group">
-                                <input className="form-control" id="phone" type="tel" placeholder="Votre téléphone *" value={formState.phone || ''} name="phone" required="required" data-validation-required-message="Please enter your phone number." onChange={changeHandler}/>
-                                <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                <textarea className="form-control" id="message" placeholder="Votre message *" value={formState.message || ''} name="message" required="required" data-validation-required-message="Please enter a message." onChange={changeHandler}></textarea>
-                                <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="clearfix"></div>
+                <Row>
+                    <Col>
+                        <div className="row">
                             <div className="col-lg-12 text-center">
-                                <div id="success"></div>
-                                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Envoyez</button>
+                                <h2 className="section-heading text-uppercase">Contactez-Nous</h2>
+                                <h3 className="section-subheading text-muted sp">Vous avez besoins des services de Jiskobou, ou tout simplement en
+                    savoir plus !?</h3>
                             </div>
-                            </div>
-                        </form>
                         </div>
-                    </div>
-                </Form>
-                </Col>
-                <Col>
-                    <Shake>
-                        <img src={phone} alt="logo" />
-                    </Shake>
-                </Col>
-            </Row>
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <Form onSubmit={handleSubmit}>
+                                    <label>
+                                    Nom et Prénom:
+                                    <input className="form-control" type="text" name="name" value={formData.name} onChange={handleChange} required />
+                                    </label>
+                                    <br />
+                                    <label>
+                                    Courriel:
+                                    <input className="form-control" type="email" name="email" value={formData.email} onChange={handleChange} required />
+                                    </label>
+                                    <br />
+                                    <label>
+                                    Téléphone:
+                                    <input className="form-control" type="tel" name="phone" value={formData.phone} onChange={handleChange} />
+                                    </label>
+                                    <br />
+                                    <label>
+                                    Message:
+                                    <textarea className="form-control" name="message" value={formData.message} onChange={handleChange} required />
+                                    </label>
+                                    <br />
+                                    <button className="btn btn-primary btn-xl text-uppercase" type="submit">Envoyez</button>
+                                </Form>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Shake>
+                            <img src={phone} alt="logo" />
+                        </Shake>
+                    </Col>
+                </Row>
             </Container>
         </section>
     </>
-  )
-}
+  );
+};
 
 export default Contact;
